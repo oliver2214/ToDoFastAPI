c5b579c3f212d1114ffc39f2fe97c1d1004132dc
oliver2214 <dima.tovstokor@yandex.ru>
2023-05-03 18:16:00 +0300

    release

---
+++ todo/config.py
@@ -0,0 +1,15 @@
+import os
+
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    app_name = os.getenv('NAME_APP')
+    db_url = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+    class Config:
+        env_file: str = '../.env'
+
+settings = Settings()
---
+++ todo/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean
+
+from todo.database.base import Base, engine
+
+
+class ToDo(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    is_complete = Column(Boolean, default=False)
+
+
+Base.metadata.create_all(bind=engine)
